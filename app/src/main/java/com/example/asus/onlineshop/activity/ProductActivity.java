b351b335c0a34918722f0abec9be9a020e001dac
chayan-DUET <chayan_cse10@yahoo.com>
2018-11-20 12:28:14 +0600

    Update ProductActivity.java
    Add btn for some action
    text initial

--- app/src/main/java/com/example/asus/onlineshop/activity/ProductActivity.java
+++ app/src/main/java/com/example/asus/onlineshop/activity/ProductActivity.java
@@ -2,12 +2,15 @@ package com.example.asus.onlineshop.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.asus.onlineshop.R;
 
 public class ProductActivity extends AppCompatActivity {
 
-
+Button btn;
+TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
